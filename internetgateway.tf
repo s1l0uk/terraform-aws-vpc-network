cbe315da3a99d6fec7f1f7697a49b16a97fc0acd
Paul Hardy <paul@2ndstudios.com>
2022-08-17 19:56:04 +0100

    moa4 dynamics and internet for subnets

--- internetgateway.tf
+++ internetgateway.tf
@@ -2,5 +2,5 @@
 resource "aws_internet_gateway" "igw" {
   count  = var.enable_internet_gateway ? 1 : 0
   vpc_id = aws_vpc.vpc
-  tags   = merge({ name = "${var.name}-igw-${var.region}${local.abcs[count.index]}" }, var.tags)
+  tags   = merge({ name = "${var.name}-igw-${var.region}" }, var.tags)
 }
--- subnets.tf
+++ subnets.tf
@@ -1,31 +1,9 @@
-# Create Subnets
-
-resource "aws_subnet" "subnet_public" {
-  count                   = length(var.subject_cidr_range_public)
-  vpc_id                  = aws_vpc.vpc
-  cidr_block              = var.subject_cidr_range_public[count.index]
-  availability_zone       = "${var.region}${local.abcs[count.index]}"
-  map_public_ip_on_launch = true
-  tags                    = merge({ name = "${var.name}-public-subnet-${var.region}${local.abcs[count.index]}" }, var.tags)
-}
-
-resource "aws_subnet" "subnet_mid" {
-  count                   = length(var.subject_cidr_range_mid)
-  vpc_id                  = aws_vpc.vpc
-  cidr_block              = var.subject_cidr_range_mid[count.index]
-  availability_zone       = "${var.region}${local.abcs[count.index]}"
-  map_public_ip_on_launch = false
-  tags                    = merge({ name = "${var.name}-mid-subnet-${var.region}${local.abcs[count.index]}" }, var.tags)
-}
-
-# Add additional Subnets here and key to variables
-# Can Dynamics and Maps be abused here to map ALL subnets...
-
-resource "aws_subnet" "subnet_data" {
-  count                   = length(var.subject_cidr_range_data)
-  vpc_id                  = aws_vpc.vpc
-  cidr_block              = var.subject_cidr_range_data[count.index]
-  availability_zone       = "${var.region}${local.abcs[count.index]}"
-  map_public_ip_on_launch = false
-  tags                    = merge({ name = "${var.name}-data-subnet-${var.region}${local.abcs[count.index]}" }, var.tags)
+# Create Subnets from Data
+resource "aws_subnet" "subnets" {
+  for_each                = module.subnet_addrs
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = each.value
+  availability_zone       = var.region + split(each.key, "-")[0]
+  map_public_ip_on_launch = length(regexall("public",each.key)) > 1 ? true : false
+  tags                    = merge({ name = "${each.key}-subnet-${var.region}" }, var.tags)
 }
