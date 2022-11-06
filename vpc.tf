f0338a16b5297bb089cdfa8b59d7545a2b83214d
Paul Hardy <paul@2ndstudios.com>
2022-11-06 10:59:45 +0000

    update vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,9 @@
 # Create a VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.network_cidr_range
+  instance_tenancy = "default"
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags       = merge({ Name = "${var.name}-${var.region}-vpc" }, var.tags)
 }
